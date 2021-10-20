09f92f38d203321fab5263332de1ab7753756967
@B.Gonçalves_dev_code <zezeballoon@gmail.com>
2021-10-20 21:45:47 +0100

    criação de class Post em models.py e makemigrations

--- blog_drf/blog/models.py
+++ blog_drf/blog/models.py
@@ -5,8 +5,24 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Post(models.Model):
+    STATUS = (
+        ('rascunho', 'Rascunho'),
+        ('publicado', 'Publicado'),
+    )
     titulo = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255)
     autor = models.ForeignKey(User,
                               on_delete=models.CASCADE)
     conteudo = models.TextField()
+    publicado = models.DateTimeField(default=timezone.now)
+    criado = models.DateTimeField(auto_now_add=True)
+    alterado = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=10,
+                              choices=STATUS,
+                              default='rascunho')
+
+    class Meta:
+        ordering = ('publicado',)
+
+    def __str__(self):
+        return self.titulo
